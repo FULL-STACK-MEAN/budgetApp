07755c14dc7e1f239cac759da3fe1225b0243738
getafemean <getafemean@gmail.com>
2021-06-25 18:32:22 +0200

    profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoaderComponent } from './nav/loader/loader.component';
 import { SideMenuComponent } from './nav/side-menu/side-menu.component';
 import { ToastMessagesComponent } from './nav/toast-messages/toast-messages.component';
 import { UserProfileComponent } from './nav/user-profile/user-profile.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { UserProfileComponent } from './nav/user-profile/user-profile.component'
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
--- src/app/nav/user-profile/user-profile.component.ts
+++ src/app/nav/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsersService } from 'src/app/services/users.service';
 
@@ -9,13 +10,16 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class UserProfileComponent implements OnInit {
 
+  dataRoutes: any;
   _id: string;
   user: any;
 
   constructor(private usersService: UsersService,
+              private route: ActivatedRoute,
               private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.dataRoutes = this.route.pathFromRoot;
     this.authService.getUserState()
                     .subscribe((data: any) => {
                       this._id = data._id;
