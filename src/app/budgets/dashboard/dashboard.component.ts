afd2b6eed2b74812319516ef4a8d823fa4de5d6b
getafemean <getafemean@gmail.com>
2021-06-29 20:54:45 +0200

    dates

--- src/app/budgets/dashboard/dashboard.component.ts
+++ src/app/budgets/dashboard/dashboard.component.ts
@@ -1,46 +1,86 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Chart } from 'chart.js';
+import { Budget } from 'src/app/models/budget.model';
+import { Dates } from 'src/app/models/dates.model';
+import { BudgetsService } from 'src/app/services/budgets.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  dataRoutes: any;
-  lineChart: any = [];
+    dataRoutes: any;
+    budgets: Array<Budget>;
 
-  constructor(private route: ActivatedRoute) { }
+    currentMonthTotal: number;
+    lastMonthTotal: number;
+    secondLastMonthTotal: number;
 
-  ngOnInit(): void {
-      this.dataRoutes = this.route.pathFromRoot;
-      setTimeout(() => {
-        this.loadLineChart();
-      }, 1500)
-  }
 
-  loadLineChart() {
-      const months = ['Abril 2021','Mayo 2021','Junio 2021'];
-      const totals = [23500,400,36000];
-      this.lineChart = new Chart('budgetsChart', {
-          type: 'line',
-          data: {
-              labels: months,
-              datasets: [
-                  {
-                      data: totals,
-                      borderColor: '#0a2558'
-                  }
-              ]
-          },
-          options: {
-              legend: {
-                  display: false
-              }
-          }
-      })
-  }
+    lineChart: any = [];
+
+    constructor(private route: ActivatedRoute,
+        private budgetsService: BudgetsService) { }
+
+    ngOnInit(): void {
+        this.dataRoutes = this.route.pathFromRoot;
+        this.budgetsService.getBudgets()
+            .subscribe((res: any) => {
+                this.budgets = res.budgets;
+                this.setTotal();
+                this.currentMonthTotal = this.getTotalMonth(Dates.getDates().firstDayCurrentMonth, Dates.getDates().lastDayCurrentMonth);
+                this.lastMonthTotal = this.getTotalMonth(Dates.getDates().firstDayLastMonth, Dates.getDates().lastDayLastMonth);
+                this.secondLastMonthTotal = this.getTotalMonth(Dates.getDates().firstDaySecondLastMonth, Dates.getDates().lastDaySecondLastMonth);
+                this.loadLineChart();
+            }, (err: any) => {
+                console.log(err);
+            })
+    }
+
+    setTotal() {
+        this.budgets.forEach((elem: any) => {
+            let total = 0;
+            elem.items.forEach(elem => {
+                total += elem.amount;
+            });
+            elem.total = total;
+        })
+    }
+
+    getTotalMonth(firstDay: any, lastDay: any) {
+        let totalMonth = 0;
+        this.budgets.forEach((elem: any) => {
+            if (new Date(elem.date).getTime() >= firstDay.getTime() &&
+                new Date(elem.date).getTime() < (lastDay.getTime() + 24 * 60 * 60 * 1000)) {
+                totalMonth += elem.total;
+            }
+        })
+        return totalMonth;
+    }
+
+    loadLineChart() {
+        const months = ['Abril 2021', 'Mayo 2021', 'Junio 2021'];
+        const totals = [this.secondLastMonthTotal, this.lastMonthTotal, this.currentMonthTotal];
+        this.lineChart = new Chart('budgetsChart', {
+            type: 'line',
+            data: {
+                labels: months,
+                datasets: [
+                    {
+                        data: totals,
+                        borderColor: '#0a2558'
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                }
+            }
+        })
+    }
 
 }
---
+++ src/app/models/dates.model.ts
@@ -0,0 +1,41 @@
+export class Dates {
+    static getDates() {
+        let currentMonth = new Date().getMonth();
+        let lastMonth = currentMonth - 1;
+        let secondLastMonth = currentMonth - 2;
+        let currentMonthYear = new Date().getFullYear();
+        let lastMonthYear = currentMonthYear;
+        let secondLastMonthYear = currentMonthYear;
+        if (currentMonth === 1) {
+            secondLastMonth = 11;
+            secondLastMonthYear = currentMonthYear - 1;
+        }
+        if (currentMonth === 0) {
+            lastMonth = 11;
+            secondLastMonth = 10;
+            lastMonthYear = currentMonthYear - 1;
+            secondLastMonthYear = currentMonthYear - 1;
+        }
+        return {
+            firstDayCurrentMonth: this.getDay(currentMonth, currentMonthYear).firstDay,
+            lastDayCurrentMonth: this.getDay(currentMonth, currentMonthYear).lastDay,
+            firstDayLastMonth: this.getDay(lastMonth, lastMonthYear).firstDay,
+            lastDayLastMonth: this.getDay(lastMonth, lastMonthYear).lastDay,
+            firstDaySecondLastMonth: this.getDay(secondLastMonth, secondLastMonthYear).firstDay,
+            lastDaySecondLastMonth: this.getDay(secondLastMonth, secondLastMonthYear).lastDay
+        }
+    }
+
+    static getDay(month, year) {
+        let day = new Date(year, month, 1);
+        let days = [];
+        while (day.getMonth() === month) {
+            days.push(new Date(day));
+            day.setDate(day.getDate() + 1);
+        }
+        return {
+            firstDay: days[0],
+            lastDay: days[days.length - 1]
+        }
+    }
+}
