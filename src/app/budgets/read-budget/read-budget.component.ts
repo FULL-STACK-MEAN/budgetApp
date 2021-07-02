63f31c7cf46d592642c5b9e391acda5198e640f2
getafemean <getafemean@gmail.com>
2021-07-02 17:33:19 +0200

    download

--- src/app/budgets/read-budget/read-budget.component.ts
+++ src/app/budgets/read-budget/read-budget.component.ts
@@ -20,6 +20,8 @@ export class ReadBudgetComponent implements OnInit {
   totalBudget = 0;
   user: any;
   pathPDF: string;
+  generatingPDF: boolean = false;
+  sendingEmail: boolean = false;
 
   constructor(private route: ActivatedRoute,
               private toastMessagesService: ToastMessagesService,
@@ -33,7 +35,7 @@ export class ReadBudgetComponent implements OnInit {
     this.budgetsService.getBudget(this._id)
                         .subscribe((res: any) => {
                             this.budget = res.budget;
-                            this.generatePDF();
+                            // this.generatePDF() En el caso de generar el PDF en back al entrar en el componente
                             this.usersService.getUser(res.budget.idSalesUser)
                                              .subscribe((res: any) => {
                                                  this.user = res.user;
@@ -49,30 +51,55 @@ export class ReadBudgetComponent implements OnInit {
   }
 
   generatePDF() {
+      this.generatingPDF = true;
       this.budgetsService.getPDFBudget(this._id)
                         .subscribe((res: any) => {
-                            this.pathPDF = environment.URLAPIserver + 'budgetspdf/' + this.budget.code + '.pdf';
-                            // let headers = new HttpHeaders();
-                            // headers = headers.set('Accept', 'application/pdf');
-                            // this.http.get(environment.URLAPIserver + 'budgetspdf/' + this.budget.code + '.pdf', { headers: headers, responseType: 'blob' })
-                            //          .subscribe((res: any) => {
-                            //              console.log(res)
-                            //          }, (err: any) => {
-                            //              console.log(err)
-                            //              //this.toastMessagesService.setToastMessage('warning', 'El servidor no se encuentra disponible en estos momentos');
-                            //          })
+                            // this.pathPDF = environment.URLAPIserver + 'budgetspdf/' + this.budget.code + '.pdf'; Establecimiento de path para atributo href en el caso de  usar en elemento enlace "a"
+                            // En el caso de generar el PDF a demanda desde el botón descargar:
+                            setTimeout(() => { 
+                                let headers = new HttpHeaders();
+                                headers = headers.set('Accept', 'application/pdf');
+                                this.http.get(environment.URLAPIserver + 'budgetspdf/' + this.budget.code + '.pdf', { headers: headers, responseType: 'blob' })
+                                        .subscribe((res: any) => {
+                                            const blobURL = URL.createObjectURL(res);
+                                            const link = document.createElement("a");
+                                            link.href = blobURL;
+                                            link.download = this.budget.code + '.pdf';
+                                            document.body.appendChild(link);
+                                            link.dispatchEvent(
+                                                new MouseEvent('click', {
+                                                    bubbles: true,
+                                                    cancelable: true,
+                                                    view: window
+                                                })
+                                            );
+                                            document.body.removeChild(link);
+                                            this.generatingPDF = false;
+                                        }, (err: any) => {
+                                            this.toastMessagesService.setToastMessage('warning', 'El servidor no se encuentra disponible en estos momentos');
+                                        })
+                            }, 2000)
                         }, (err: any) => {
                             this.toastMessagesService.setToastMessage('warning', 'El servidor no se encuentra disponible en estos momentos');
                         })
   }
 
   sendBudgetEmail() {
-      this.budgetsService.getSendBudgetEmail(this._id)
-                        .subscribe((res: any) => {
-                            console.log(res);
-                        }, (err: any) => {
-                            this.toastMessagesService.setToastMessage('warning', 'El servidor no se encuentra disponible en estos momentos');
-                        })
+        this.sendingEmail = true;
+        this.budgetsService.getPDFBudget(this._id)
+                    .subscribe((res: any) => {
+                        setTimeout(() => {
+                            this.budgetsService.getSendBudgetEmail(this._id)
+                                                .subscribe((res: any) => {
+                                                    this.sendingEmail = false;
+                                                    this.toastMessagesService.setToastMessage('success', res.message);
+                                                }, (err: any) => {
+                                                    this.toastMessagesService.setToastMessage('warning', 'El servidor no se encuentra disponible en estos momentos');
+                                                })  
+                        }, 2000)
+                    }, (err: any) => {
+                        this.toastMessagesService.setToastMessage('warning', 'El servidor no se encuentra disponible en estos momentos');
+                    })
   }
 
 }
